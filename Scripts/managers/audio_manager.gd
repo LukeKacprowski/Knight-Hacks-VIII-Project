3ba5d3de171b898253cff93bfdd07a1956582918
Mustapha324 <mustapha324@outlook.com>
2025-10-25 02:27:29 -0400

    Add new music and SFX assets, update audio setup
    Added new background music and sound effects, removed Player2wins music, and updated import files accordingly. Introduced a new music scene and autoload, updated audio manager to preload the new music, and added a default audio bus layout for improved audio management.

--- Scripts/managers/audio_manager.gd
+++ Scripts/managers/audio_manager.gd
@@ -1,9 +1,10 @@
 extends Node
 
+const MUSIC_FOR_GAME = preload("uid://d2amf71lry4x1")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
