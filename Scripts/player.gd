7bb12902ab33c0bd286372e7e1c160332814abaa
Luke Kacprowski <lukekac@me.com>
2025-10-25 09:36:39 -0400

    Finalizing Merge Fixes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
+
 extends Node
 class_name PlayerData
 
 var player_id: int = 0
-=======
-extends Node 
-class_name PlayerData
-
-var player_id: int
->>>>>>> parent of 19dab6c (Merge branch 'main' of https://github.com/LukeKacprowski/Knight-Hacks-VIII-Project)
 var player_name: String = ""
 
 var lives: int = 3
 var max_lives: int = 3
 
-<<<<<<< HEAD
 signal lives_changed(new_lives: int)
 signal player_died
 
-=======
->>>>>>> parent of 19dab6c (Merge branch 'main' of https://github.com/LukeKacprowski/Knight-Hacks-VIII-Project)
 func setup(id: int, name: String):
 	player_id = id
 	player_name = name
