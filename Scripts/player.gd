218827e2806470d7a1a906522597ab9e1875eb25
Luke Kacprowski <lukekac@me.com>
2025-10-25 09:04:00 -0400

    Enhance PlayerData with signals and setup method
    Added class_name PlayerData, player properties, and new signals for lives changes and player death. Introduced setup and reset_lives methods, and updated take_damage to emit signals. Registered InputManager in project.godot and added UID files for new scripts.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,16 +1,30 @@
 extends Node
+class_name PlayerData
 
+var player_id: int = 0
+var player_name: String = ""
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var lives: int = 3
+var max_lives: int = 3
 
+signal lives_changed(new_lives: int)
+signal player_died
+
+func setup(id: int, name: String):
+	player_id = id
+	player_name = name
+	reset_lives()
 
 func take_damage():
 	lives -= 1
+	emit_signal("lives_changed", lives)
+	
+	if lives <= 0:
+		emit_signal("player_died")
 
 func reset_lives():
 	lives = max_lives
+	emit_signal("lives_changed", lives)
 
 func get_lives():
 	return lives
