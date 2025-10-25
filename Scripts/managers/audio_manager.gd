58ca49312b09cb7a9acf775368040e56b94b3b56
Mustapha324 <mustapha324@outlook.com>
2025-10-25 04:10:39 -0400

    Add dynamic audio players and sound trigger methods
    Dynamically creates AudioStreamPlayer nodes for various game sounds and adds them to the scene. Implements methods to play win, hit, and sword clash sounds, and loads the corresponding audio files in _ready().

--- Scripts/managers/audio_manager.gd
+++ Scripts/managers/audio_manager.gd
@@ -1,12 +1,39 @@
 extends Node
 
-const MUSIC_FOR_GAME = preload("uid://d2amf71lry4x1")
+# Create audio players dynamically
+@onready var player1_win_sound = AudioStreamPlayer.new()
+@onready var player2_win_sound = AudioStreamPlayer.new()
+@onready var player_hit_sound = AudioStreamPlayer.new()
+@onready var sword_clash_sound = AudioStreamPlayer.new()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+func _ready():
+	# Add all to scene
+	add_child(player1_win_sound)
+	add_child(player2_win_sound)
+	add_child(player_hit_sound)
+	add_child(sword_clash_sound)
 
+	# Load your sounds
+	player1_win_sound.stream = load("res://Player1wins.mp3")
+	player2_win_sound.stream = load("res://Player2wins.mp3")
+	player_hit_sound.stream = load("res://Playerhit.mp3")
+	sword_clash_sound.stream = load("res://Swordconnecthit.mp3")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# --- Trigger Functions ---
+
+func play_player1_win():
+	if player1_win_sound:
+		player1_win_sound.play()
+
+func play_player2_win():
+	if player2_win_sound:
+		player2_win_sound.play()
+
+func play_player_hit():
+	if player_hit_sound:
+		player_hit_sound.play()
+
+func play_sword_clash():
+	if sword_clash_sound:
+		sword_clash_sound.play()
+		
