f569ff329ae854cc66b84576c0cda1fd52db512b
Mustapha324 <mustapha324@outlook.com>
2025-10-25 22:51:37 -0400

    Add main menu button and update stats scene
    Added a 'Main Menu' button to the Stats scene with custom styles and connected its signal to a new handler in stats.gd. Updated stats.gd to extend Node instead of Panel, refactored stats update logic for clarity, and fixed a typo in how_to_play2.tscn instructions.

--- Scripts/stats.gd
+++ Scripts/stats.gd
@@ -1,4 +1,4 @@
-extends  Panel
+extends  Node
 
 @onready var total_games: Label = $VBoxContainer/total_games
 @onready var total_round_wins: Label = $"VBoxContainer/total_round wins"
@@ -15,3 +15,22 @@ func _ready():
 
 func update_stats():
 	var s = StatsManager.get_stats()
+	
+	#total stats
+	total_games.text = "Total Games: " + str(s["total_games"])
+	total_round_wins.text = "Total Round Wins: " + str(s["total_rounds"])
+	round_count.text = "Longest Round Streak: " + str(s["round_counter"])
+
+	#Player 1 stats
+	p_1_gw.text = "P1 Game Wins: " + str(s["Player1"]["game_wins"])
+	p_1_rw.text = "P1 Round Wins: " + str(s["Player1"]["round_wins"])
+	p_1_flawless.text = "P1 Flawless: " + str(s["Player1"]["flawless_victories"])
+
+	# ✅ Player 2 stats
+	p_2_gw.text = "P2 Game Wins: " + str(s["Player2"]["game_wins"])
+	p_2_rw.text = "P2 Round Wins: " + str(s["Player2"]["round_wins"])
+	p_2_flawless.text = "P2 Flawless: " + str(s["Player2"]["flawless_victories"])
+
+
+func _on_main_menu_pressed() -> void:
+	GameManager.goto("main_menu")
