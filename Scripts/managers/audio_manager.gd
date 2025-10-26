ce4e968f0564401723bdd84a82635bfa06727460
Mustapha324 <mustapha324@outlook.com>
2025-10-26 03:05:39 -0400

    Add new sword clash sound effects and randomize playback
    Added three new sword clash audio files and their import settings. Updated audio_manager.gd to load and manage these new sounds, and modified the play_sword_clash function to randomly play one of the new sword clash effects for more audio variety.

--- Scripts/managers/audio_manager.gd
+++ Scripts/managers/audio_manager.gd
@@ -8,8 +8,14 @@ extends Node
 @onready var menu_music = AudioStreamPlayer.new()
 @onready var game_music = AudioStreamPlayer.new()
 
+@onready var sword_clash_one = AudioStreamPlayer.new()
+@onready var sword_clash_two = AudioStreamPlayer.new()
+@onready var sword_clash_three = AudioStreamPlayer.new()
+
+
 
 func _ready():
+	randomize()
 	# Add all to scene
 	add_child(player1_win_sound)
 	add_child(player2_win_sound)
@@ -17,6 +23,10 @@ func _ready():
 	add_child(sword_clash_sound)
 	add_child(menu_music)
 	add_child(game_music)
+	
+	add_child(sword_clash_one)
+	add_child(sword_clash_two)
+	add_child(sword_clash_three)
 
 
 
@@ -25,6 +35,11 @@ func _ready():
 	player2_win_sound.stream = load("res://Assets/audio/sfx/Player2wins.mp3")
 	player_hit_sound.stream = load("res://Assets/audio/sfx/Playerhit.mp3")
 	sword_clash_sound.stream = load("res://Assets/audio/sfx/Swordclashboth.mp3")
+	
+	#sword clashes
+	sword_clash_one.stream = load("res://Assets/audio/sfx/Slashnew1.mp3")
+	sword_clash_two.stream = load("res://Assets/audio/sfx/Slashnew2.mp3")
+	sword_clash_three.stream = load("res://Assets/audio/sfx/Slashnew3.mp3")
 	
 	#Load Backgroundmusic
 	menu_music.stream = load("res://Assets/audio/music/Menumusic.mp3")
@@ -49,7 +64,11 @@ func play_player_hit():
 
 func play_sword_clash():
 	if sword_clash_sound:
-		sword_clash_sound.play()
+		var rn = randi_range(0, 1)
+		if rn == 0:
+			sword_clash_one.play()
+		elif rn==1:
+			sword_clash_three.play()
 
 #Background music functions
 func play_menu_music():
