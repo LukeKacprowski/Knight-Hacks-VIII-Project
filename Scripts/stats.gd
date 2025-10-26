a85558ca36f9f1a4044054e423b2959bdb98f810
Justin Suriel <justinmsuriel@gmail.com>
2025-10-25 22:22:07 -0400

    Add stats UI and connect labels in Stats scene
    Introduced multiple Label nodes to the Stats scene for displaying various statistics. Updated stats.gd to reference these labels and added an update_stats() function to populate them, preparing the UI for dynamic stat updates.

--- Scripts/stats.gd
+++ Scripts/stats.gd
@@ -1,11 +1,17 @@
-extends Control
+extends  Panel
 
+@onready var total_games: Label = $VBoxContainer/total_games
+@onready var total_round_wins: Label = $"VBoxContainer/total_round wins"
+@onready var round_count: Label = $VBoxContainer/Round_count
+@onready var p_1_gw: Label = $VBoxContainer/P1_GW
+@onready var p_1_rw: Label = $VBoxContainer/P1_RW
+@onready var p_1_flawless: Label = $VBoxContainer/P1_flawless
+@onready var p_2_gw: Label = $VBoxContainer/P2_GW
+@onready var p_2_rw: Label = $VBoxContainer/P2_RW
+@onready var p_2_flawless: Label = $VBoxContainer/P2_flawless
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	update_stats()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func update_stats():
+	var s = StatsManager.get_stats()
